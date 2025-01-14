968a20369b389cecdea741aa244364b918fb1b15
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-14 10:26:44 +0100

    Doxygen Init

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-01-14
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <ESP8266WiFi.h>
