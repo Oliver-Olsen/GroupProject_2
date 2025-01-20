db40b10b93cb75a427f2ff55eecf52cc73cabb5d
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-21 00:26:17 +0100

    receiver setup identified instabillity caused by stepper module

--- src/main.cpp
+++ src/main.cpp
@@ -13,24 +13,8 @@
 
 #include <Arduino.h>
 #include "sendRecieveData.h"
-#include "airQual.h"
-#include "tempHumi.h"
-#include "motionSensor.h"
-#include "dataLCD.h"
 #include "recieverModule.h"
 
-
-#define SENDING_STATION   true
-
-
-float temperature = 0;
-float humidity    = 0;
-float airquality  = 0;
-bool motion_state = false;  // Initial state of the room.
-bool motion_val   = false;  // Intial Value of the Sensor.
-int windowCh = 5;
-int heaterCh = 7;
-int lightCh = 6;
 
 
 unsigned short int seconds_15 = 15000; // ThingSpeak read/write speed (free subscription)
@@ -46,13 +30,10 @@ int update_web = 0;
  */
 void setup() {
 
-  //Serial.begin(115200);
+  Serial.begin(115200);
     wifi_init();
-    motionSensor_init(); // PIR motion sensor sat as input.
-    tempHumi_Init();
-    dataLCD_setup();
 
-    receiverModule_init(windowCh, heaterCh, lightCh);
+    receiverModule_init();
 
 
   //Serial.println("Setup Complete");
@@ -67,36 +48,14 @@ void setup() {
  */
 void loop()
 {
-  //Serial.print("Loop count");
-  //Serial.println(update_web);
-
-  //if (SENDING_STATION == true){
-
-    switch (update_web)
+switch (update_web)
     {
     case THINGSPEAK_AIRQUALITY:
-        tempHumi_read(&temperature, &humidity);
-        airQual_measurement(&temperature, &humidity);
-        dataLCD_print(airQual_get_Value(), THINGSPEAK_AIRQUALITY);
-        sendData_fieldValue(THINGSPEAK_AIRQUALITY, airQual_get_Value());
-        break;
-
     case THINGSPEAK_MOTION:
-        motionSensor_detect(&motion_state, &motion_val);
-        sendData_fieldValue(THINGSPEAK_MOTION, 1);
-        break;
-
     case THINGSPEAK_TEMPERATURE:
-        tempHumi_read(&temperature, &humidity);
-        dataLCD_print(temperature, THINGSPEAK_TEMPERATURE);
-        sendData_fieldValue(THINGSPEAK_TEMPERATURE, temperature);
-        break;
-
     case THINGSPEAK_HUMIDITY:
-        tempHumi_read(&temperature, &humidity);
-        dataLCD_print(humidity, THINGSPEAK_HUMIDITY);
-        sendData_fieldValue(THINGSPEAK_HUMIDITY, humidity);
-        break;
+        receiverModule_update(update_web);
+
 
     default:
         break;
@@ -106,6 +65,10 @@ void loop()
     {
         update_web = 0;
     }
+
     delay(seconds_15);
     update_web++;
+
+    Serial.print(".");
 }
+
--- src/sendRecieveData.cpp
+++ src/sendRecieveData.cpp
@@ -16,7 +16,6 @@
 #define CONNECT_PORT 80
 
 #define CHANNEL_NUMBER 2810501UL
-const char Write_APIKey[] = "6475AR5ZBC5ZU3A7";
 const char Read_APIKey[] = "AMPXQDNAKUDP9COD";
 
 const char* ssid = "Connection Error";
@@ -40,78 +39,11 @@ void wifi_init()
 
 /**
  * @author Oliver Olsen
- * @brief Handles float to int
- *
- * @see works with @p sendData_fieldValue()
- * @param field
- * @param data
- */
-void sendData_fieldValue(int field, float data)
-{
-  int value = (int)(data * 100);
-  sendData_payload(field, value, CHANNEL_NUMBER, Write_APIKey);
-}
-
-
-/**
- * @author Oliver Olsen
- * @brief Handles int to int
- *
- * @section The two functions @p sendData_fieldValue() ans @p sendData_fieldValue(). Deppending on if it's an int or a float, the correct function will be chosen automatically.
- * @see works with @p sendData_fieldValue()
- * @param field
- * @param data
- */
-void sendData_fieldValue(int field, int data)
-{
-  sendData_payload(field, data, CHANNEL_NUMBER, Write_APIKey);
-}
-
-
-/**
-* @author Marcus & Emil
-* @brief sends data to our Thingspeak cloud (Requires WiFi.Begin in setup)
-* @section field 1 = CO2 , field 2 = Sound, field 3 = Temperature, field 4 Humidity
-* @param field which Thingspeak field the data is sent to (1-8)
-* @param data data to be sent
-* @param channelID The channel ID
-* @param Write_APIKey The key to write to Thingspeak
-*/
-void sendData_payload(int field,int data, unsigned long channelID,const char *Write_APIKey)
-{
-  connectTingSpeak();
-  ThingSpeak.setField(field, data);
-  ThingSpeak.writeFields(channelID, Write_APIKey);
-  // Delay of 5 seconds to make sure the data is recieved correctly by thingspeak
-  //delay(5000);
-}
-
-
-/**
-* @author Marcus & Emil
-* @brief recieves data from Thingspeak (Requires WiFi.Begin in setup)
-* @param field which Thingspeak field the data is read from (1-8)
-* @param channelID The channel ID
-* @param Read_APIKey The key to read to Thingspeak
-* field 5 = window motor control, field 6 = sound led control
-*/
-int recievedata(int field,long channelID,const char *Read_APIKey)
-{
-  connectTingSpeak();
-  return ThingSpeak.readIntField(channelID, field, Read_APIKey); //returns the read data from Thingspeak
-  //Delay of 30 seconds to avoid using too many
-  //delay(30000);
-}
-
-
-
-/**
- * @author Oliver Olsen
  * @brief Similar to @p recieveData()
  * @section used to smoothly merge with recieverModule.cpp
  * @see recievedata()
  */
-void connectTingSpeak()
+void connectThingSpeak()
 {
   client.connect("api.thingspeak.com", CONNECT_PORT);
 }
@@ -131,13 +63,3 @@ int readThingSpeak(int field)
   return ThingSpeak.readIntField(CHANNEL_NUMBER, field, Read_APIKey);
 }
 
-
-/**
- * @brief Ends the connection
- * @author Oliver Olsen
- * @section Currently not used
- */
-void sendData_finished()
-{
-    client.stop();
-}
