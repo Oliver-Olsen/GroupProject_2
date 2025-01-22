863238f6237b1698cc9133dc30e5e4c457b31b55
Metafysical <wulff43211234@gmail.com>
2025-01-22 10:12:17 +0100

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /**
  * @file main.cpp
- * @author 
+ * @author
  * @brief
  * @version 0.1
  * @date 2025-01-14
@@ -13,30 +13,14 @@
 
 #include <Arduino.h>
 #include "sendRecieveData.h"
-#include "airQual.h"
-#include "tempHumi.h"
-#include "motionSensor.h"
-#include "dataLCD.h"
 #include "recieverModule.h"
 
-
-#define SENDING_STATION   true
-
-
-float temperature = 0;
-float humidity    = 0;
-float airquality  = 0;
-bool motion_state = false;  // Initial state of the room.
-bool motion_val   = false;  // Intial Value of the Sensor.
-int windowCh = 5;
-int heaterCh = 7;
-int lightCh = 6;
 
 
 unsigned short int seconds_15 = 15000; // ThingSpeak read/write speed (free subscription)
 
 
-int update_web = 0;
+int update_web = 5;
 
 
 /**
@@ -46,16 +30,12 @@ int update_web = 0;
  */
 void setup() {
 
-  //Serial.begin(115200);
-  wifi_init();
-  motionSensor_init(); // PIR motion sensor sat as input.
-  tempHumi_Init();
-  dataLCD_setup();
-  
-  receiverModule_init(windowCh, heaterCh, lightCh);
-  
+    Serial.begin(115200);
+    wifi_init();
 
-  //Serial.println("Setup Complete");
+    receiverModule_init();
+
+    Serial.begin(115200);
 }
 
 
@@ -63,54 +43,30 @@ void setup() {
 /**
  * @author Oliver Olsen
  * @brief Updates ThingSpeak or gets data from ThingSpeak
- * @section Updates ThingSpeak every 15 seconds if sending station. If receiving end, gets data every 15 seconds.  
+ * @section Updates ThingSpeak every 15 seconds if sending station. If receiving end, gets data every 15 seconds.
  */
 void loop()
 {
-  //Serial.print("Loop count");
-  //Serial.println(update_web);
-
-  //if (SENDING_STATION == true){
-    
-    switch (update_web)
+switch (update_web)
     {
-    case THINGSPEAK_AIRQUALITY:
-      tempHumi_read(&temperature, &humidity);
-      airQual_measurement(&temperature, &humidity);
-      dataLCD_print(airQual_get_Value(), THINGSPEAK_AIRQUALITY);
-      sendData_fieldValue(THINGSPEAK_AIRQUALITY, airQual_get_Value());
-      
-      
-      //Serial.print(temperature, humidity);
-      
-      
-      break;
+    case 5:
+    case 6:
+    case 7:
+        receiverModule_update(update_web);
 
-    case THINGSPEAK_MOTION:
-      motionSensor_detect(&motion_state, &motion_val);
-      sendData_fieldValue(THINGSPEAK_MOTION, 1);
-      break;
-
-    case THINGSPEAK_TEMPERATURE:
-      tempHumi_read(&temperature, &humidity);
-      dataLCD_print(temperature, THINGSPEAK_TEMPERATURE);
-      sendData_fieldValue(THINGSPEAK_TEMPERATURE, temperature);
-      break;
-
-    case THINGSPEAK_HUMIDITY:
-      tempHumi_read(&temperature, &humidity);
-      dataLCD_print(humidity, THINGSPEAK_HUMIDITY);
-      sendData_fieldValue(THINGSPEAK_HUMIDITY, humidity);
-      break;
 
     default:
-      break;
+        break;
     }
 
-    if (update_web >= THINGSPEAK_MAX)
+    if (update_web >= 7)
     {
-      update_web = 0;
+        update_web = 4;
+        Serial.println(update_web); //Serial readout used for debugging
     }
-      delay(seconds_15);
-      update_web++;
+
+    delay(seconds_15);
+    update_web++;
+
+    Serial.println(".");
 }
