8948087e0df73488f5710dfae8b149fc54ff7fec
Metafysical <wulff43211234@gmail.com>
2025-01-22 10:54:08 +0100

    Added all relevant code for receiver module to the main branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,34 @@
 /**
  * @file main.cpp
- * @author
- * @brief
+ * @author your name (you@domain.com)
+ * @brief 
  * @version 0.1
- * @date 2025-01-14
- *
+ * @date 2025-01-22
+ * 
  * @copyright Copyright (c) 2025
- *
+ * 
  */
 
 
 
 #include <Arduino.h>
-#include "sendRecieveData.h"
-#include "recieverModule.h"
+#include "receiverMain.h"
 
 
+bool transmitter = true; 
 
-unsigned short int seconds_15 = 15000; // ThingSpeak read/write speed (free subscription)
-
-
-int update_web = 5;
-
-
-/**
- * @author Oliver olsen
- * @brief Init of sensors and modules
- * @section Inits the correct elements, depending on if the station is on the recieving end or sending end
- */
 void setup() {
-
     Serial.begin(115200);
-    wifi_init();
-
-    receiverModule_init();
-
-    Serial.begin(115200);
+    receiverSetup(); 
+    //Transmitter setup
 }
 
 
-
-/**
- * @author Oliver Olsen
- * @brief Updates ThingSpeak or gets data from ThingSpeak
- * @section Updates ThingSpeak every 15 seconds if sending station. If receiving end, gets data every 15 seconds.
- */
 void loop()
 {
-switch (update_web)
-    {
-    case 5:
-    case 6:
-    case 7:
-        receiverModule_update(update_web);
-
-
-    default:
-        break;
+    if (transmitter == true) {
+        //Transmitter function
+    }  else {
+        receiverMain(); 
     }
-
-    if (update_web >= 7)
-    {
-        update_web = 4;
-        Serial.println(update_web); //Serial readout used for debugging
-    }
-
-    delay(seconds_15);
-    update_web++;
-
-    Serial.println(".");
 }
--- src/stepperPlus.h
+++ src/stepperPlus.h
@@ -1,20 +1,20 @@
 /**
  * @file stepper.h
  * @author Nils Linus Metsälä Wulff (s223968@student.dtu.dk)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2025-01-21
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
 
 #ifndef STEPPERPLUS_H
 #define STEPPERPLUS_H
 
 //Initializes custom functions
-void stepWrite(int step); 
-void stepperInit(); 
-void stepperRotate(int rotation); 
+void stepWrite(int step);
+void stepperInit();
+void stepperRotate(int rotation);
 
 #endif // STEPPERPLUS_H
