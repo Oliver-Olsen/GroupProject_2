a26abd34e865a3bb264a34fac30ea9efc59992c5
Embo02 <s224023@student.dtu.dk>
2025-01-22 09:04:06 +0100

    added the matlab visual + manual overwrite

--- Thingspeak matlab/ThingspeakHeaterControl.m
+++ Thingspeak matlab/ThingspeakHeaterControl.m
@@ -1,26 +1,31 @@
 
 % Channel ID to read data from 
-ChannelID = 2810501; 
-% Temperature Field ID  
+ChannelIDRead = 2810501; 
+ChannelIDWrite = 2815518;
+% Channel Read API Key  
+readAPIKey = 'AMPXQDNAKUDP9COD'; 
+% Enter the Write API Key between the '' below: 
+writeAPIKey = 'UZG6FLYTIIJSFHSA'; 
+
+
+
 TempID = 3;
 HeaterID = 7;
 Time = 10;
 
-% Channel Read API Key  
-readAPIKey = 'AMPXQDNAKUDP9COD'; 
-% Enter the Write API Key between the '' below: 
-writeAPIKey = '6475AR5ZBC5ZU3A7'; 
+
+
 Motorrotation = 0;
 
-DeciTemp = thingSpeakRead(ChannelID,'Fields',TempID,'ReadKey',readAPIKey,NumMinutes= Time);
+DeciTemp = thingSpeakRead(ChannelIDRead,'Fields',TempID,'ReadKey',readAPIKey,NumMinutes= Time);
 Temp = mean(DeciTemp)/10;
 
 
 
-if (Temp > 26)
-    Motorrotation = 5;
-elseif (Temp < 22)
-     Motorrotation = -5;
+if (Temp > 24)
+    Motorrotation = -1;
+elseif (Temp < 20)
+     Motorrotation = 1;
 else
      Motorrotation = 0;
 end
@@ -30,5 +35,7 @@ end
 % Learn more about the THINGSPEAKWRITE function by going to the Documentation tab on 
 % the right side pane of this page. 
 
-pause(20);
-thingSpeakWrite(ChannelID,'Fields',HeaterID,'Values',Motorrotation,'WriteKey',writeAPIKey)
+%Motorrotation = 1
+
+pause(5);
+thingSpeakWrite(ChannelIDWrite,'Fields',HeaterID,'Values',Motorrotation,'WriteKey',writeAPIKey)
---
+++ Thingspeak matlab/Visual/Tempdisplay.m
@@ -0,0 +1,31 @@
+
+% Channel ID to read data from 
+ChannelID = 2810501; 
+% Temperature Field ID 
+TempID = 3; 
+
+% Channel Read API Key  
+readAPIKey = 'AMPXQDNAKUDP9COD'; 
+% Enter the Write API Key between the '' below: 
+writeAPIKey = '6475AR5ZBC5ZU3A7'; 
+
+%reads data
+[data,time] = thingSpeakRead(ChannelID, 'Field', TempID, 'ReadKey', readAPIKey,NumPoints=100);
+temp = data./100;
+
+%finds NAN and replaces it
+for i = 1:length(temp)
+    if isnan(temp(i))
+        temp(i) = -256;
+    end
+end
+
+%shows time 
+scatter(time,temp)
+ylim([0,40])
+%Adds info bar
+cmap = parula(256)  % Blue at bottom, red at top.
+colormap((cmap));  % Apply colormap
+colorbar;
+caxis([0, 40]);
+ylabel('Degrees \circC') 
